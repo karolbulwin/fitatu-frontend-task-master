7a659d40f349b7a720d721f0a55814659e579441
Karol Bulwin <karolbulwin@gmail.com>
2019-07-13 21:26:13 +0200

    edit button component

---
+++ src/components/EmployeesList/EditButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="employees-list__edit-button" @click="handleClick">{{text}}</button>
+</template>
+<script>
+export default {
+  props: ["text"],
+  methods: {
+    handleClick() {
+      this.$emit("click");
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.employees-list__edit-button {
+  background-color: transparent;
+  border: 1px solid #000000;
+  color: #000000;
+  border-radius: 0.2em;
+  transition-duration: 300ms;
+  padding: 0.3em 0.7em;
+  cursor: pointer;
+  &:hover {
+    color: white;
+    background-color: #000000;
+  }
+}
+</style>
